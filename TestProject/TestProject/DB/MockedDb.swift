5347c83a340e627efc18138430b83159c7d449fc
alexander.martirosov <a_martirosov@icloud.com>
2024-10-10 20:21:19 +0400

    Update API endpoint to include user authentication
    Changed the API endpoint to require user authentication for access. This enhancement ensures that only authenticated users can access sensitive data, improving overall security. Adjusted the request handling to validate user credentials before granting access.

--- TestProject/TestProject/DB/MockedDb.swift
+++ TestProject/TestProject/DB/MockedDb.swift
@@ -9,18 +9,18 @@ import Foundation
 
 class MockedDb {
     static var nodes = Set<DbNode>()
-    
+
     static func populateDb() {
         // Adding the root node
         let id1 = UUID()
         nodes.insert(DbNode.mock(id: id1, parentId: nil, name: 1))
-        
+
         // Level 2
         let id2 = UUID()
         nodes.insert(DbNode.mock(id: id2, parentId: id1, name: 2))
         let id3 = UUID()
         nodes.insert(DbNode.mock(id: id3, parentId: id1, name: 3))
-        
+
         // Level 3
         let id4 = UUID()
         nodes.insert(DbNode.mock(id: id4, parentId: id2, name: 4))
@@ -30,7 +30,7 @@ class MockedDb {
         nodes.insert(DbNode.mock(id: id6, parentId: id3, name: 6))
         let id7 = UUID()
         nodes.insert(DbNode.mock(id: id7, parentId: id3, name: 7))
-        
+
         // Level 4
         let id8 = UUID()
         nodes.insert(DbNode.mock(id: id8, parentId: id4, name: 8))
@@ -61,7 +61,7 @@ private extension DbNode {
     static func mock(id: UUID, parentId: UUID?, name: Int) -> DbNode {
         return .init(id: id, parentId: parentId, value: "I'm node with id: \(name)")
     }
-    
+
     private static func mapUUIDToNumber(uuid: UUID, range: ClosedRange<Int> = 0...1000) -> Int {
         // Convert UUID to a string and hash it to a numeric value
         let uuidString = uuid.uuidString
